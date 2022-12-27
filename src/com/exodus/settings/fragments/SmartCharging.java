e4c400e353147beba2cd76104bccf3d748c8283a
Sensei-Developer <111665604+Sensei-Developer@users.noreply.github.com>
2022-12-27 09:07:25 +0300

    Update SmartCharging.java

--- src/com/exodus/settings/fragments/SmartCharging.java
+++ src/com/exodus/settings/fragments/SmartCharging.java
@@ -63,6 +63,6 @@ public class SmartCharging extends SettingsPreferenceFragment {
 
     @Override
     public int getMetricsCategory() {
-        return MetricsProto.MetricsEvent.CRDROID_SETTINGS;
+        return MetricsProto.MetricsEvent.EXODUS_CUSTOMISATION;
     }
 }
